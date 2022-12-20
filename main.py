c455fcf36b44a40ebe0c958fa4a694b895370108
haskirloh <seniaveretenov07@mail.ru>
2022-12-21 01:01:31 +0200

    CAMERA
    ОДНА ОШИБКА= 2 ГОДА ЗАНЯТИЙ С ПСИХОЛОГОМ(ПРОВЕРЕНО)

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from pygame import gfxdraw
 pg.init()
 true_width, true_height = pygame.display.Info().current_w, pygame.display.Info().current_h
 print(true_width, true_height)
-WIDTH = true_width * 0.8
-HEIGHT = true_height * 0.8
+WIDTH = int(true_width * 0.8)
+HEIGHT = int(true_height * 0.8)
 FPS = 60
 running = True
 clock = pg.time.Clock()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 ASSETS_PATH = 'Assets/'
 controls = [[pg.K_w, pg.K_d, pg.K_s, pg.K_a, pg.K_SPACE, pg.KMOD_SHIFT]]
+G = 2
 
 
 class Main:
@@ -80,18 +81,50 @@ class Camera:
     def __init__(self):
         self.cam_x = 0
         self.cam_y = 0
+
         print('camera initialized')
 
     def update(self):
-        pass
+        # self.cam_x = int(len(game.map.hitboxes) - WIDTH)
+        # self.cam_y = int(len(game.map.hitboxes[0]) - HEIGHT)
         # print(self.cam_x, self.cam_y)
+        ...
+
+    def set(self, x, y):
+        ans = [0, 0]
+        if x >= game.map.size[0] - WIDTH:
+            print('horizontal border ->')
+            ans[0] = 1
+        if x < 0:
+            print('horizontal border <-')
+            ans[0] = -1
+        if ans[0] == 0:
+            self.cam_x = x
+        if y >= game.map.size[1] - HEIGHT:
+            print('vertical border ->')
+            ans[1] = 1
+        if y < 0:
+            print('vertical border <-')
+            ans[1] = -1
+        if ans[1] == 0:
+            self.cam_y = y
+        return ans
 
     def move(self, x, y):
-        if self.cam_x >= game.map.size[0] - WIDTH or self.cam_y >= game.map.size[1] - HEIGHT:
-            print('граница камеры')
+        '''ans = [0, 0]
+        if x >= game.map.size[0] - WIDTH:
+            print('horizontal border')
+            ans[0] = 1
         else:
             self.cam_x += x
+        if y >= game.map.size[1] - HEIGHT:
+            print('vertical border')
+            ans[1] = 1
+        else:
             self.cam_y += y
+
+        return ans'''
+        ...
 
 
 class Object:
@@ -101,13 +134,46 @@ class Object:
 
 class Pawn:
     def __init__(self):
+        self.truecords = (WIDTH // 2, HEIGHT // 2)
         self.x, self.y = 0, 0
         pygame.sprite.Sprite.__init__(self)
         self.group = pygame.sprite.Group()
         self.group.add(self)
         self.controls_num = 0
-        self.horizontal_speed = 3
-        self.vertical_speed = 10
+        self.horizontal_speed = 0
+        self.vertical_speed = 0
+        self.grav_accel = G
+        self.onfloor = 0
+        self.lascoords = (None, None)
+        self.cam_xlock, self.cam_ylock = 0, 0
+        self.xcamflg, self.ycamflg = 0, 0
+
+    def cam_targeting(self):
+        tx, ty = self.truecords
+        r1, r2 = game.camera.cam_x if self.xcamflg else self.x, game.camera.cam_y if self.ycamflg else self.y
+        x = game.camera.set(r1, r2)
+        camx, camy = game.camera.cam_x, game.camera.cam_x
+        print(x)
+        if x[0] == 1 or self.rect.x > tx:
+            self.xcamflg = 1
+            self.rect.x = self.x - camx + tx
+        elif x[0] == -1 or self.rect.x < tx:
+            self.xcamflg = -1
+            self.rect.x = self.x - camx + tx
+        else:
+            self.xcamflg = 0
+            print('nicex')
+            # self.rect.x = tx
+        if x[1] == 1 or self.rect.y > ty:
+            self.ycamflg = 1
+            self.rect.y = self.y - camy + ty
+        elif x[1] == -1 or self.rect.y < ty:
+            self.ycamflg = -1
+            self.rect.y = self.y - camy + ty
+        else:
+            self.ycamflg = 0
+            print('nicey')
+            # self.rect.y = ty
 
     def events_check(self):
         keyboard = pygame.key.get_pressed()
@@ -119,7 +185,23 @@ class Pawn:
         self.control(keys)
 
     def physics(self):
-        ...
+        self.y += self.vertical_speed
+        self.x += self.horizontal_speed
+        y2 = self.rect.bottom
+        self.onfloor = 0
+        for x in range(self.rect.bottomleft[0], self.rect.bottomright[0]):
+            for y in range(y2, y2 + self.grav_accel + G):
+                # print(y)
+                if game.map.visible_area[x][y]:
+                    self.onfloor = 1
+                    f = 1
+        print(self.onfloor)
+        if self.onfloor:
+            self.grav_accel = G
+            self.vertical_speed = 0
+        else:
+            # self.grav_accel += G
+            self.vertical_speed += self.grav_accel
 
 
 class Human(Pawn, pygame.sprite.Sprite):
@@ -130,21 +212,24 @@ class Human(Pawn, pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.runanimation = [animpath + f'{i}.png' for i in range(1, 7)]
         self.moves = []
-        print(self.runanimation)
+        print(self.rect.bottom)
 
     def update(self):
+        self.cam_targeting()
+        # self.physics()
         self.events_check()
+        # self.rect.x, self.rect.y = self.x, self.y
         self.group.draw(screen)
 
     def control(self, keys):
         if 0 in keys:
-            pass
+            self.y -= 5
         if 1 in keys:
-            pass
+            self.x += 5
         if 2 in keys:
-            pass
+            self.y += 5
         if 3 in keys:
-            pass
+            self.x -= 5
         if 4 in keys:
             pass
 
