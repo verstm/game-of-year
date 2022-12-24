d5eb4e959cb9be9c897c01e947906de4f3ab2539
haskirloh <seniaveretenov07@mail.ru>
2022-12-24 14:06:38 +0200

    прога для генерации хитбоксов

---
+++ test.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import numpy
+
+img = Image.open('ht.png')
+hitboxes = numpy.zeros((img.size[0], img.size[1]))
+for i in range(img.size[0]):
+    for j in range(img.size[1]):
+        pix = img.load()[i, j]
+        if pix[0] == 0:
+            hitboxes[i][j] = 1
+print(len(hitboxes))
+numpy.savetxt('xd.txt', hitboxes)
