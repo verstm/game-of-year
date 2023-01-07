2f36fec066fd3592312944bbf39b2d0606d8d2d0
Kalix228 <seniaveretenov07@mail.ru>
2023-01-07 20:35:30 +0200

    x2 sprite size

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-'''from PIL import Image
+from PIL import Image
 import numpy
 
 img = Image.open('ht.png')
@@ -9,8 +9,4 @@ for i in range(img.size[0]):
         if pix[0] != 255:
             hitboxes[i][j] = 1
 print(len(hitboxes))
-numpy.savetxt('xd.txt', hitboxes)'''
-a = [[3, 2, 1]]
-b = [[1, 2, 3]]
-a[0][0] = b[0][0]
-print(a, b)
+numpy.savetxt('xd.txt', hitboxes)
